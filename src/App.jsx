3ed670993df533b2e305aaf57e2052d68a426f08
Esrafil <esrafil.s.r.man418@gmail.com>
2026-06-09 19:25:23 +0330

    feat: Add a State to save cards in it

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Card from "./components/Card";
 import GameHeader from "./components/GameHeader";
 
@@ -21,13 +22,46 @@ const cardValues = [
 ];
 
 export default function App() {
+  const [cards, setCards] = useState([]);
+
+  const initializeGame = () => {
+    // Shuffle the Cards
+
+    const finalCards = cardValues.map((value, index) => ({
+      id: index,
+      value,
+      isFlipped: false,
+      isMatched: false,
+    }));
+    setCards(finalCards);
+  };
+
+  useEffect(() => {
+    initializeGame();
+  }, []);
+
+  const handleCardClick = (card) => {
+    // Don't allow clicking if card is already flipped, matched
+    if (card.isFlipped || card.isMatched) {
+      return;
+    }
+    // Update card flipped state
+    const newCards = cards.map((c) => {
+      if (c.id === card.id) {
+        return { ...c, isFlipped: true };
+      } else {
+        return c;
+      }
+    });
+    setCards(newCards);
+  };
   return (
     <div className="app">
       <GameHeader score={2} moves={8} />
 
       <div className="cards-grid">
-        {cardValues.map((card) => (
-          <Card card={card} />
+        {cards.map((card) => (
+          <Card key={card.id} card={card} onClick={handleCardClick} />
         ))}
       </div>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,11 @@
-export default function Card({ card }) {
+export default function Card({ card, onClick }) {
   return (
-    <div className="card">
-        <div className="card-front">?</div>
-      <div className="card-back">{card}</div>
+    <div
+      className={`card ${card.isFlipped ? "flipped" : ""}`}
+      onClick={() => onClick(card)}
+    >
+      <div className="card-front">?</div>
+      <div className="card-back">{card.value}</div>
     </div>
   );
 }
